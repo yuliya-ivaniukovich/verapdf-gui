b94e052a3d69503b9915c2a2cc1ed1bed8aec7c0
Aleksandr Fomin <RezviyBelorus@gmail.com>
2018-02-06 12:44:28 +0300

    Button file input added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-// import 'bootstrap/dist/css/bootstrap.css'
+
+import App from './components/App';
 
 ReactDOM.render(
   <App />,
