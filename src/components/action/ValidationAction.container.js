7094ac47efa6a433bc287305be35760cdad40c07
Alexey Nelipovich <nelipovich1992@gmail.com>
2018-09-25 12:53:04 +0300

    Created and passed ValidationAction component in children props

--- src/components/action/ValidationAction.container.js
+++ src/components/action/ValidationAction.container.js
@@ -1,23 +1,43 @@
-import {connect} from 'react-redux';
-import {toggleValidationActive} from '../../redux/modules/configuration/validation';
-import Action from './Action';
+import React from "react";
+import { connect } from "react-redux";
 
+import { validationActions } from "../../redux/modules/configuration/validation";
+import ValidationAction from "./ValidationAction";
+import Action from "./Action";
 
 const mapStateToProps = state => {
     return {
         checked: state.configuration.validation.active,
         title: "Validation",
         collapsible: true,
-        children: null
+        children: (
+            <ValidationAction
+                profilesValues={
+                    state.validationProfilesValues.validationProfiles
+                }
+                profile={state.configuration.validation.profile}
+                profilePath={state.configuration.validation.profileFilePath}
+                prefix={state.configuration.validation.prefix}
+                amountFails={state.configuration.validation.amountFails}
+                includeInformation={
+                    state.configuration.validation.includeInformationInReport
+                }
+                isFixMetadata={state.configuration.validation.fixMetadata}
+                stopValidation={state.configuration.validation.stopValidation}
+            />
+        )
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
         onToggleActive: () => {
-            dispatch(toggleValidationActive());
+            dispatch(validationActions.toggleValidationActive());
         }
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Action);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Action);
