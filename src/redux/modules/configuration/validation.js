80aae12295b143b21b6490302ede6282cdbe2863
Alexey Nelipovich <nelipovich1992@gmail.com>
2018-09-25 12:55:13 +0300

    Created action creators and added types in reducer

--- src/redux/modules/configuration/validation.js
+++ src/redux/modules/configuration/validation.js
@@ -1,16 +1,68 @@
-import {createAction, handleActions} from 'redux-actions';
+import { createAction, handleActions } from 'redux-actions';
 
 //- Actions
-export const toggleValidationActive = createAction('VALIDATION_ACTIVE_TOGGLE');
+export const validationActions = {
+  toggleValidationActive: createAction('VALIDATION_ACTIVE_TOGGLE'),
+  selectFixMetadata: createAction('SELECT_FIX_METADATA'),
+  selectIncludeInformation: createAction('SELECT_INCLUDE_INFORMATION'),
+  stopValidation: createAction('SELECT_STOP_VALIDATION'),
+  selectValidationProfile: createAction('SELECT_VALIDATION_PROFILE'),
+  getProfilePath: createAction('GET_PROFILE_PATH'),
+  selectAmountFails: createAction('SELECT_AMOUNT_FAILS'),
+  selectPrefix: createAction('SELECT_PREFIX')
+};
 
 //- State
 const initialState = {
-    active: false
+  active: false,
+  profile: 'custom',
+  profileFilePath: null,
+  fixMetadata: false,
+  prefix: 'veraFixMd_',
+  includeInformationInReport: false,
+  stopValidation: false,
+  amountFails: 100
 };
 
 //- Reducers
-export default handleActions({
-    VALIDATION_ACTIVE_TOGGLE: (state) => {
-        return {...state, active: !state.active};
+export default handleActions(
+  {
+    VALIDATION_ACTIVE_TOGGLE: state => {
+      return { ...state, active: !state.active };
+    },
+    SELECT_VALIDATION_PROFILE: (state, action) => {
+      return { ...state, profile: action.payload };
+    },
+    GET_PROFILE_PATH: (state, action) => {
+      return { ...state, profileFilePath: action.payload };
+    },
+    SELECT_FIX_METADATA: state => {
+      return { ...state, fixMetadata: !state.fixMetadata };
+    },
+    SELECT_PREFIX: (state, action) => {
+      return {
+        ...state,
+        prefix: action.payload
+      };
+    },
+    SELECT_INCLUDE_INFORMATION: state => {
+      return {
+        ...state,
+        includeInformationInReport: !state.includeInformationInReport
+      };
+    },
+    SELECT_STOP_VALIDATION: state => {
+      return {
+        ...state,
+        stopValidation: !state.stopValidation
+      };
+    },
+    SELECT_AMOUNT_FAILS: (state, action) => {
+      return {
+        ...state,
+        amountFails: action.payload
+      };
     }
-}, initialState);
+  },
+  initialState
+);
