a561e01d653b2262a11ea6599e14a154f90724e7
Stanislau <onestepway@gmail.com>
2018-09-26 11:08:59 +0300

    add action, reducer for getting policy file path

--- src/redux/modules/configuration/policy.js
+++ src/redux/modules/configuration/policy.js
@@ -1,16 +1,23 @@
-import {createAction, handleActions} from 'redux-actions';
+import { createAction, handleActions } from 'redux-actions';
 
 //- Actions
 export const togglePolicyActive = createAction('POLICY_ACTIVE_TOGGLE');
-
+export const getPolicyFilePath = createAction('GET_POLICY_FILE_PATH');
 //- State
 const initialState = {
-    active: false
+    active: false,
+    policyFilePath: ''
 };
 
 //- Reducers
-export default handleActions({
-    POLICY_ACTIVE_TOGGLE: (state) => {
-        return {...state, active: !state.active};
-    }
-}, initialState);
+export default handleActions(
+    {
+        POLICY_ACTIVE_TOGGLE: state => {
+            return { ...state, active: !state.active };
+        },
+        GET_POLICY_FILE_PATH: (state, action) => {
+            return { ...state, policyFilePath: action.payload };
+        }
+    },
+    initialState
+);
