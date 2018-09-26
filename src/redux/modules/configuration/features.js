1c5e990eae4b1bb3267cc9e841207b695a17fbaa
Stanislau <onestepway@gmail.com>
2018-09-26 11:08:11 +0300

    add action, reducer for adding selected features

--- src/redux/modules/configuration/features.js
+++ src/redux/modules/configuration/features.js
@@ -1,16 +1,26 @@
-import {createAction, handleActions} from 'redux-actions';
+import { createAction, handleActions } from 'redux-actions';
 
 //- Actions
 export const toggleFeaturesActive = createAction('FEATURES_ACTIVE_TOGGLE');
+export const addSelectedFeatures = createAction('ADD_SELECTED_FEATURES');
 
 //- State
 const initialState = {
-    active: false
+    active: false,
+    selectedFeatures: [
+        { value: 'information dictionary', label: 'Information Dictionary' }
+    ]
 };
 
 //- Reducers
-export default handleActions({
-    FEATURES_ACTIVE_TOGGLE: (state) => {
-        return {...state, active: !state.active};
-    }
-}, initialState);
+export default handleActions(
+    {
+        FEATURES_ACTIVE_TOGGLE: state => {
+            return { ...state, active: !state.active };
+        },
+        ADD_SELECTED_FEATURES: (state, action) => {
+            return { ...state, selectedFeatures: action.payload };
+        }
+    },
+    initialState
+);
