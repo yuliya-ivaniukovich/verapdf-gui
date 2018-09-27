f82e4e91ae3c2f67742fc0aef19f519fee60d334
Alexey Nelipovich <nelipovich1992@gmail.com>
2018-09-27 13:18:00 +0300

    Renamed actions name and types

--- src/redux/modules/configuration/validation.js
+++ src/redux/modules/configuration/validation.js
@@ -2,67 +2,67 @@ import { createAction, handleActions } from 'redux-actions';
 
 //- Actions
 export const validationActions = {
-  toggleValidationActive: createAction('VALIDATION_ACTIVE_TOGGLE'),
-  selectFixMetadata: createAction('SELECT_FIX_METADATA'),
-  selectIncludeInformation: createAction('SELECT_INCLUDE_INFORMATION'),
-  stopValidation: createAction('SELECT_STOP_VALIDATION'),
-  selectValidationProfile: createAction('SELECT_VALIDATION_PROFILE'),
-  getProfilePath: createAction('GET_PROFILE_PATH'),
-  selectAmountFails: createAction('SELECT_AMOUNT_FAILS'),
-  selectPrefix: createAction('SELECT_PREFIX')
+    toggleValidationActive: createAction('VALIDATION_ACTIVE_TOGGLE'),
+    toggleFixMetadata: createAction('FIX_METADATA_TOGGLE'),
+    toggleIncludeInformation: createAction('INCLUDE_INFORMATION_TOGGLE'),
+    toggleStopValidation: createAction('VAIDATION_STOP_TOGGLE'),
+    selectValidationProfile: createAction('VALIDATION_PROFILE_SELECT'),
+    getProfilePath: createAction('PROFILE_GET_PATH'),
+    selectAmountFails: createAction('AMOUNT_FAILS_SELECT'),
+    selectPrefix: createAction('SELECT_PREFIX')
 };
 
 //- State
 const initialState = {
-  active: false,
-  profile: 'custom',
-  profileFilePath: null,
-  fixMetadata: false,
-  prefix: 'veraFixMd_',
-  includeInformationInReport: false,
-  stopValidation: false,
-  amountFails: 100
+    active: false,
+    profile: 'custom',
+    profileFilePath: null,
+    fixMetadata: false,
+    prefix: 'veraFixMd_',
+    includeInformationInReport: false,
+    stopValidation: false,
+    amountFails: 100
 };
 
 //- Reducers
 export default handleActions(
-  {
-    VALIDATION_ACTIVE_TOGGLE: state => {
-      return { ...state, active: !state.active };
+    {
+        VALIDATION_ACTIVE_TOGGLE: state => {
+            return { ...state, active: !state.active };
+        },
+        VALIDATION_PROFILE_SELECT: (state, action) => {
+            return { ...state, profile: action.payload };
+        },
+        PROFILE_GET_PATH: (state, action) => {
+            return { ...state, profileFilePath: action.payload };
+        },
+        FIX_METADATA_TOGGLE: state => {
+            return { ...state, fixMetadata: !state.fixMetadata };
+        },
+        SELECT_PREFIX: (state, action) => {
+            return {
+                ...state,
+                prefix: action.payload
+            };
+        },
+        INCLUDE_INFORMATION_TOGGLE: state => {
+            return {
+                ...state,
+                includeInformationInReport: !state.includeInformationInReport
+            };
+        },
+        VAIDATION_STOP_TOGGLE: state => {
+            return {
+                ...state,
+                stopValidation: !state.stopValidation
+            };
+        },
+        AMOUNT_FAILS_SELECT: (state, action) => {
+            return {
+                ...state,
+                amountFails: action.payload
+            };
+        }
     },
-    SELECT_VALIDATION_PROFILE: (state, action) => {
-      return { ...state, profile: action.payload };
-    },
-    GET_PROFILE_PATH: (state, action) => {
-      return { ...state, profileFilePath: action.payload };
-    },
-    SELECT_FIX_METADATA: state => {
-      return { ...state, fixMetadata: !state.fixMetadata };
-    },
-    SELECT_PREFIX: (state, action) => {
-      return {
-        ...state,
-        prefix: action.payload
-      };
-    },
-    SELECT_INCLUDE_INFORMATION: state => {
-      return {
-        ...state,
-        includeInformationInReport: !state.includeInformationInReport
-      };
-    },
-    SELECT_STOP_VALIDATION: state => {
-      return {
-        ...state,
-        stopValidation: !state.stopValidation
-      };
-    },
-    SELECT_AMOUNT_FAILS: (state, action) => {
-      return {
-        ...state,
-        amountFails: action.payload
-      };
-    }
-  },
-  initialState
+    initialState
 );
