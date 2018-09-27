cbf56e1366d99f0206853a8d1eec05c523614a2b
Alexey Nelipovich <nelipovich1992@gmail.com>
2018-09-27 13:32:24 +0300

    Renemed validationProfilesValues to validationProfiles

--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -1,10 +1,10 @@
-import { combineReducers } from "redux";
-import configuration from "./configuration";
-import filesToValidate from "./filesToValidate";
-import validationProfilesValues from "./validationProfilesValues";
+import { combineReducers } from 'redux';
+import configuration from './configuration';
+import filesToValidate from './filesToValidate';
+import validationProfiles from './validationProfiles';
 
 export const rootReducer = combineReducers({
     configuration,
     filesToValidate,
-    validationProfilesValues
+    validationProfiles
 });
