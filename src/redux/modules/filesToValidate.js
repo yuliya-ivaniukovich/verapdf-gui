6df9cfea6457ce04a39dc8b6d535048f327737e4
Alexey Nelipovich <nelipovich1992@gmail.com>
2018-10-10 18:51:31 +0300

    Add action addFiles

--- src/redux/modules/filesToValidate.js
+++ src/redux/modules/filesToValidate.js
@@ -1,8 +1,42 @@
 import {createAction, handleActions} from 'redux-actions';
 import _ from 'lodash';
 
+
+const createFormData = files => {
+    const body = new FormData()
+    body.append('file', files[0])
+    return body;
+}
+
+const createFileList = response => {
+    if(response.message === 'File already exists') {
+        alert('File already exists, please choose a new file');
+        return new Error('File already exists')
+    }
+     const file = _.chain([response])
+            .keyBy('fileId')
+            .mapValues(() => ({type: 'pdf', path: response.path}))
+            .value()
+    return file
+}
+
 //- Actions
-export const addFiles = createAction('FILES_TO_VALIDATE_ADD', files => files);
+const addFilesRequest = createAction('FILES_TO_VALIDATE_ADD_REQUEST')
+const addFilesSuccess = createAction('FILES_TO_VALIDATE_ADD_SUCCESS')
+
+export const addFiles = (files) => {
+        return (dispatch, getState)=> {
+            const state = getState();
+            dispatch(addFilesRequest());
+            fetch(`http://localhost:8080/api/jobs/${state.job.id}/files`, {
+                method: 'POST',
+                body: createFormData(files)
+            })
+                .then(response => response.json())
+                .then(response => dispatch(addFilesSuccess(createFileList(response))))
+                .catch(error => console.error(error))
+        }
+};
 export const delFile = createAction('FILE_TO_VALIDATE_DEL');
 
 //- State
@@ -10,7 +44,7 @@ const initialState = {};
 
 //- Reducers
 export default handleActions({
-    FILES_TO_VALIDATE_ADD: (state, action) => {
+    FILES_TO_VALIDATE_ADD_SUCCESS: (state, action) => {
         return {
             ...state,
             ..._.mapValues(action.payload, file => ({...file, status: null}))
