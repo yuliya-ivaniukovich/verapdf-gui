c49d95a3c11c253c483f346961aa1dd9c3958b67
Alexey Nelipovich <nelipovich1992@gmail.com>
2018-10-10 18:50:06 +0300

    Import job file

--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import configuration from './configuration';
 import filesToValidate from './filesToValidate';
 import validationProfiles from './validationProfiles';
+import job from './job';
 
 export const rootReducer = combineReducers({
+    job,
     configuration,
     filesToValidate,
     validationProfiles
