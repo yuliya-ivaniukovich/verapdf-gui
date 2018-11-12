fd8071c661ba0a4f4ae6e9e3df3df0764f27b5df
Stanislau <grench333@gmail.com>
2018-11-12 10:00:43 +0300

    move electron check to separate file, update uploadFile method, clean up

--- src/api/files.js
+++ src/api/files.js
@@ -1,17 +1,25 @@
 import {handleResponse, defaultUrl} from './base';
+import { isElectron } from '../enviroment';
 
 export const FilesApi = {
     uploadFile(jobId, file) {
-        const body = new FormData();
-        body.append('file', file);
-        return fetch(`${defaultUrl}/api/jobs/${jobId}/files`, { method: 'POST', body }).then(handleResponse);
-    },
-    uploadLocalFile(jobId, filePath) {
-        const body = JSON.stringify({type: 'LOCAL', path: filePath});
-        return fetch(`${defaultUrl}/api/jobs/${jobId}/files`, {
-            headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          }, method: 'POST', body }).then(handleResponse);
+        if (isElectron) {
+            const body = JSON.stringify({type: 'LOCAL',path: file.path});
+            return fetch(`${defaultUrl}/api/jobs/${jobId}/files`, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: 'POST',
+                body
+            }).then(handleResponse);
+        } else {
+            const body = new FormData();
+            body.append('file', file);
+            return fetch(`${defaultUrl}/api/jobs/${jobId}/files`, {
+                method: 'POST', 
+                body
+            }).then(handleResponse);
+        }
     }
 }; 
